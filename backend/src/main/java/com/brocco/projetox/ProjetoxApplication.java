a27c988253bbd2cc2735ca6297971f7c5c71afec
Fernando Brocco <fernandobroccox1@gmail.com>
2022-07-01 18:13:26 -0300

    Projeto criado

---
+++ backend/src/main/java/com/brocco/projetox/ProjetoxApplication.java
@@ -0,0 +1,13 @@
+package com.brocco.projetox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoxApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/brocco/projetox/ProjetoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brocco.projetox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
